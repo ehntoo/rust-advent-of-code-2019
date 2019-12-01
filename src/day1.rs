b7291497ecd00bb0a27623516cf672152addb1fa
Mitchell Johnson <ehntoo@gmail.com>
2019-12-01 13:00:01 -0500

    Add a fold-based solution
    According to godbolt, it produces the same assembly in release mode
    Go figure.

--- src/day1.rs
+++ src/day1.rs
@@ -3,9 +3,14 @@ pub fn input_generator(input: &str) -> Vec<i32> {
     input.lines().map(|l| l.parse().unwrap()).collect()
 }
 
-#[aoc(day1, part1)]
+#[aoc(day1, part1, map)]
 pub fn solve_part_1(module_masses: &[i32]) -> i32 {
     module_masses.iter().map(|i| (i/3)-2).sum()
+}
+
+#[aoc(day1, part1, fold)]
+pub fn solve_part_1_fold(module_masses: &[i32]) -> i32 {
+    module_masses.iter().fold(0, |acc, i| acc + ((i/3)-2))
 }
 
 #[aoc(day1, part2)]
@@ -23,7 +28,7 @@ pub fn solve_part_2(module_masses: &[i32]) -> i32 {
 
 #[cfg(test)]
 mod tests {
-    use super::solve_part_1 as part1;
+    use super::solve_part_1_fold as part1;
     use super::solve_part_2 as part2;
 
     #[test]
