9d0fbcf9998766c2ff87958fe1c900c0e989dde9
Mitchell Johnson <ehntoo@gmail.com>
2019-12-02 09:15:34 -0500

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -4,24 +4,45 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 }
 
 #[aoc(day2, part1)]
-pub fn part1(intcode: &[i32]) -> i32 {
-    let mut local_program = intcode.to_vec();
+pub fn part1(program: &[i32]) -> i32 {
+    let mut local_program = program.to_vec();
     local_program[1] = 12;
     local_program[2] = 2;
+    run_program(&mut local_program)
+}
+
+#[aoc(day2, part2)]
+pub fn part2(program: &[i32]) -> i32 {
+    let target_output = 19690720;
+    for noun in 0..100 {
+        for verb in 0..100 {
+            let mut local_program = program.to_vec();
+            local_program[1] = noun;
+            local_program[2] = verb;
+            let result = run_program(&mut local_program);
+            if result == target_output {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    -1
+}
+
+pub fn run_program(intcode: &mut Vec<i32>) -> i32 {
     let mut pc = 0;
     loop {
-        let opcode = local_program[pc];
-        let src_1 = local_program[pc + 1] as usize;
-        let src_2 = local_program[pc + 2] as usize;
-        let dst = local_program[pc + 3] as usize;
+        let opcode = intcode[pc];
+        let src_1 = intcode[pc + 1] as usize;
+        let src_2 = intcode[pc + 2] as usize;
+        let dst = intcode[pc + 3] as usize;
         match opcode {
-            1 => local_program[dst] = local_program[src_1] + local_program[src_2],
-            2 => local_program[dst] = local_program[src_1] * local_program[src_2],
+            1 => intcode[dst] = intcode[src_1] + intcode[src_2],
+            2 => intcode[dst] = intcode[src_1] * intcode[src_2],
             _ => break,
         };
         pc += 4;
     }
-    local_program[0]
+    intcode[0]
 }
 
 #[cfg(test)]
