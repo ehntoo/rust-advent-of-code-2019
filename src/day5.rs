1ae29870769df911f76e6b910086442658a965f1
Mitchell Johnson <ehntoo@gmail.com>
2019-12-05 07:56:23 -0500

    Solve part 2

--- src/day5.rs
+++ src/day5.rs
@@ -3,10 +3,6 @@ pub fn input_generator(input: &str) -> Vec<i32> {
     input.split(',').map(|l| l.parse().unwrap()).collect()
 }
 
-pub fn read_int() -> i32 {
-    return 1;
-}
-
 pub fn write_int(int: i32) {
     println!("{}", int);
 }
@@ -19,27 +15,36 @@ pub fn decode_arg(mode: i32, pc: usize, intcode: &[i32]) -> i32 {
     }
 }
 
-pub fn run_program(intcode: &mut Vec<i32>) -> i32 {
+pub fn run_program(intcode: &mut Vec<i32>, input: i32) -> i32 {
     let mut pc = 0;
     loop {
-        // println!("Processing opcode {}, arg1 {} arg2 {} arg3 {}", intcode[pc], intcode[pc+1], intcode[pc+2], intcode[pc+3]);
         let opcode = intcode[pc] % 100;
         let arg1_mode = (intcode[pc] / 100) % 10;
         let arg2_mode = (intcode[pc] / 1000) % 10;
-        // let arg3_mode = intcode[pc] / 10000;
         let raw_arg_1 = intcode[pc+1];
-        let raw_arg_3 = intcode[pc+3];
-        // println!("arg1_mode {} arg2_mode {} arg3_mode {}", arg1_mode, arg2_mode, arg3_mode);
-        // println!("{}", intcode[225]);
-        // println!("{:?}", intcode);
         let decode_arg1 = || decode_arg(arg1_mode, pc+1, intcode);
         let decode_arg2 = || decode_arg(arg2_mode, pc+2, intcode);
-        // let _arg3 = decode_arg(arg3_mode, pc+3, intcode);
         match opcode {
-            1 => {intcode[raw_arg_3 as usize] = decode_arg1() + decode_arg2(); pc += 4},
-            2 => {intcode[raw_arg_3 as usize] = decode_arg1() * decode_arg2(); pc += 4},
-            3 => {intcode[raw_arg_1 as usize] = read_int(); pc += 2},
+            1 => {
+                let dst = intcode[pc+3] as usize;
+                intcode[dst] = decode_arg1() + decode_arg2(); pc += 4
+            },
+            2 => {
+                let dst = intcode[pc+3] as usize;
+                intcode[dst] = decode_arg1() * decode_arg2(); pc += 4
+            },
+            3 => {intcode[raw_arg_1 as usize] = input; pc += 2},
             4 => {write_int(decode_arg1()); pc += 2},
+            5 => pc = if decode_arg1() != 0 { decode_arg2() as usize} else { pc + 3 },
+            6 => pc = if decode_arg1() == 0 { decode_arg2() as usize} else { pc + 3 },
+            7 => {
+                let dst = intcode[pc+3] as usize;
+                intcode[dst] = if decode_arg1() < decode_arg2() {1} else {0}; pc += 4
+            },
+            8 => {
+                let dst = intcode[pc+3] as usize;
+                intcode[dst] = if decode_arg1() == decode_arg2() {1} else {0}; pc += 4
+            },
             _ => break,
         };
     }
@@ -49,15 +54,13 @@ pub fn run_program(intcode: &mut Vec<i32>) -> i32 {
 #[aoc(day5, part1)]
 pub fn part1(program: &[i32]) -> i32 {
     let mut local_program = program.to_vec();
-    run_program(&mut local_program)
+    run_program(&mut local_program, 1)
 }
 
 #[aoc(day5, part2)]
 pub fn part2(program: &[i32]) -> i32 {
     let mut local_program = program.to_vec();
-    local_program[1] = 12;
-    local_program[2] = 2;
-    run_program(&mut local_program)
+    run_program(&mut local_program, 5)
 }
 
 #[cfg(test)]
