57f1fcb26b5261ae567c2a86aafb1bb078e3c9b1
Mitchell Johnson <ehntoo@gmail.com>
2019-12-07 15:02:14 -0500

    Restructure intcode computer to allow for continuing execution

--- src/day7.rs
+++ src/day7.rs
@@ -13,50 +13,57 @@ pub fn decode_arg(mode: i32, pc: usize, intcode: &[i32]) -> i32 {
     }
 }
 
-pub fn run_program(intcode: &mut Vec<i32>, input: &[i32]) -> Vec<i32> {
-    let mut pc = 0;
-    let mut input_idx = 0;
-    let mut output = vec![];
-    loop {
-        let opcode = intcode[pc] % 100;
-        let arg1_mode = (intcode[pc] / 100) % 10;
-        let arg2_mode = (intcode[pc] / 1000) % 10;
-        let arg1 = || decode_arg(arg1_mode, pc+1, intcode);
-        let arg2 = || decode_arg(arg2_mode, pc+2, intcode);
+pub struct IntCodeComputerState {
+    prog: Vec<i32>,
+    pc: usize,
+    halted: bool,
+}
+
+pub fn run_program(state: &mut IntCodeComputerState, input: i32) -> i32 {
+    let mut output = 0;
+    while !state.halted {
+        let opcode = state.prog[state.pc] % 100;
+        let arg1_mode = (state.prog[state.pc] / 100) % 10;
+        let arg2_mode = (state.prog[state.pc] / 1000) % 10;
+        let arg1 = || decode_arg(arg1_mode, state.pc+1, &state.prog);
+        let arg2 = || decode_arg(arg2_mode, state.pc+2, &state.prog);
         match opcode {
             1 => {
-                let dst = intcode[pc+3] as usize;
-                intcode[dst] = arg1() + arg2();
-                pc += 4
+                let dst = state.prog[state.pc+3] as usize;
+                state.prog[dst] = arg1() + arg2();
+                state.pc += 4
             },
             2 => {
-                let dst = intcode[pc+3] as usize;
-                intcode[dst] = arg1() * arg2();
-                pc += 4
+                let dst = state.prog[state.pc+3] as usize;
+                state.prog[dst] = arg1() * arg2();
+                state.pc += 4
             },
             3 => {
-                let dst = intcode[pc+1] as usize;
-                intcode[dst] = input[input_idx];
-                input_idx += 1;
-                pc += 2
+                let dst = state.prog[state.pc+1] as usize;
+                state.prog[dst] = input;
+                state.pc += 2;
+                break;
             },
             4 => {
-                output.push(arg1());
-                pc += 2
+                output = arg1();
+                state.pc += 2;
+                break;
             },
-            5 => pc = if arg1() != 0 { arg2() as usize} else { pc + 3 },
-            6 => pc = if arg1() == 0 { arg2() as usize} else { pc + 3 },
+            5 => state.pc = if arg1() != 0 { arg2() as usize} else { state.pc + 3 },
+            6 => state.pc = if arg1() == 0 { arg2() as usize} else { state.pc + 3 },
             7 => {
-                let dst = intcode[pc+3] as usize;
-                intcode[dst] = if arg1() < arg2() {1} else {0};
-                pc += 4
+                let dst = state.prog[state.pc+3] as usize;
+                state.prog[dst] = if arg1() < arg2() {1} else {0};
+                state.pc += 4
             },
             8 => {
-                let dst = intcode[pc+3] as usize;
-                intcode[dst] = if arg1() == arg2() {1} else {0};
-                pc += 4
+                let dst = state.prog[state.pc+3] as usize;
+                state.prog[dst] = if arg1() == arg2() {1} else {0};
+                state.pc += 4
             },
-            _ => break,
+            _ => {
+                state.halted = true
+            },
         };
     }
     output
@@ -66,21 +73,54 @@ pub fn run_program(intcode: &mut Vec<i32>, input: &[i32]) -> Vec<i32> {
 pub fn part1(program: &[i32]) -> i32 {
     let mut max_output = 0;
     for perm in (0..5).permutations(5) {
-        let out = run_program(&mut program.to_vec(), &vec![perm[0], 0]);
-        let out = run_program(&mut program.to_vec(), &vec![perm[1], out[0]]);
-        let out = run_program(&mut program.to_vec(), &vec![perm[2], out[0]]);
-        let out = run_program(&mut program.to_vec(), &vec![perm[3], out[0]]);
-        let out = run_program(&mut program.to_vec(), &vec![perm[4], out[0]]);
-        if out[0] > max_output {
-            max_output = out[0];
+        let mut amplifier_computers: Vec<IntCodeComputerState> = (0..5).map(|_| {
+            IntCodeComputerState {
+                prog: program.to_vec(),
+                pc: 0,
+                halted: false,
+            }
+        }).collect();
+        run_program(&mut amplifier_computers[0], perm[0]);
+        run_program(&mut amplifier_computers[0], 0);
+        let out = run_program(&mut amplifier_computers[0], 0);
+        run_program(&mut amplifier_computers[1], perm[1]);
+        run_program(&mut amplifier_computers[1], out);
+        let out = run_program(&mut amplifier_computers[1], out);
+        run_program(&mut amplifier_computers[2], perm[2]);
+        run_program(&mut amplifier_computers[2], out);
+        let out = run_program(&mut amplifier_computers[2], out);
+        run_program(&mut amplifier_computers[3], perm[3]);
+        run_program(&mut amplifier_computers[3], out);
+        let out = run_program(&mut amplifier_computers[3], out);
+        run_program(&mut amplifier_computers[4], perm[4]);
+        run_program(&mut amplifier_computers[4], out);
+        let out = run_program(&mut amplifier_computers[4], out);
+        if out > max_output {
+            max_output = out;
         }
     }
     max_output
 }
 
 #[aoc(day7, part2)]
-pub fn part2(program: &[i32]) -> i32 {
-    let mut local_program = program.to_vec();
-    run_program(&mut local_program, &vec![0, 5]);
+pub fn part2(_program: &[i32]) -> i32 {
+    // let mut max_output = 0;
+    // for perm in (5..10).permutations(5) {
+    //     let mut amplifier_computers: Vec<IntCodeComputerState> = (0..5).map(|_| {
+    //         IntCodeComputerState {
+    //             prog: program.to_vec(),
+    //             pc: 0,
+    //         }
+    //     }).collect();
+    //     let out = run_program(&mut amplifier_computers[0], &vec![perm[0], 0]);
+    //     let out = run_program(&mut amplifier_computers[1], &vec![perm[1], out[0]]);
+    //     let out = run_program(&mut amplifier_computers[2], &vec![perm[2], out[0]]);
+    //     let out = run_program(&mut amplifier_computers[3], &vec![perm[3], out[0]]);
+    //     let out = run_program(&mut amplifier_computers[4], &vec![perm[4], out[0]]);
+    //     if out[0] > max_output {
+    //         max_output = out[0];
+    //     }
+    // }
+    // max_output
     0
 }
